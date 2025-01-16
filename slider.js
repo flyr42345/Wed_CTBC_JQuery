cc7dfc26ed938649b4c290b0c000e5646e0b8b08
flyr42345 <linj56290@gmail.com>
2025-01-16 13:31:59 +0800

    Upte圖片網頁完成

--- slider.js
+++ slider.js
@@ -1,5 +1,11 @@
 // 當網頁載入完畢後準備完成後在執行箭頭函式
 
 $(document).ready(()=>{
-    console.log("網頁下載完畢")
-})
+    // 點擊事件:點下任何一個 .slider都會執行
+    $(".slider").click(function () {
+        // 刪除全部的 .slider類別 active
+        $(".slider").removeClass("active");
+        // 添加點下去的 .slider類別
+        $(this).addClass("active");
+    });
+});
